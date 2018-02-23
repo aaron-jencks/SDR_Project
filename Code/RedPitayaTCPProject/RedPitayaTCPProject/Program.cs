95fc2307880f0cdc3c0c481fb454119dce4b48e6
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-02-23 12:11:55 -0600

    Added a red pitaya
    Added a visual studio project for communicating with the red pitaya

---
+++ Code/RedPitayaTCPProject/RedPitayaTCPProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace RedPitayaTCPProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TcpClient client = new TcpClient();
+            client.Connect("rp-f04948.local", 5000);
+            NetworkStream stream = client.GetStream();
+            Thread RxThread = new Thread(RxManager);
+            RxThread.Start();
+
+            while(client.Connected)
+            {
+                string entry = Console.ReadLine();
+                stream.Write(Encoding.ASCII.GetBytes(entry), 0, entry.Length);
+            }
+
+            void RxManager()
+            {
+                byte[] message = new byte[256];
+                while(client.Connected)
+                {
+                    int count = stream.Read(message, 0, message.Length);
+                    if(count != 0)
+                    {
+                        stream.Read(message, 0, count);
+                        Console.WriteLine(Encoding.ASCII.GetString(message));
+                    }
+                }
+            }
+        }
+    }
+}
